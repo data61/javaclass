757a5f0d9b79214866fa68471d2e076bf7ff2a66
Tony Morris <tmorris@tmorris.net>
2016-09-22 14:20:48 +1000

    Version

--- src/Language/Java/Class/Version.hs
+++ src/Language/Java/Class/Version.hs
@@ -2,85 +2,61 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TemplateHaskell #-}
 
-module Language.Java.Class.Version {-(
+module Language.Java.Class.Version(
   Version(..)
+, HasVersion(..)
 , VersionError(..)
-, AsVersionMinorUnexpectedEof(..)
+, HasVersionError(..)
+, AsVersionError(..)
 , versionMinorUnexpectedEof
-, AsVersionMajorUnexpectedEof(..)
 , versionMajorUnexpectedEof
-, version
-) -} where
+, getVersion
+) where
 
-import Control.Applicative(Applicative)
-import Control.Lens(Choice, Optic', prism', ( # ))
-import Control.Monad(return)
-import Data.Eq(Eq)
-import Data.Functor.Identity(Identity)
-import Data.Maybe(Maybe(Just, Nothing))
-import Data.Ord(Ord)
-import Data.Tagged(Tagged)
 import Data.Tickle(Get, word16be, (!-))
 import Data.Word(Word16)
-import Prelude(Show)
+import Papa
 
 -- |
 --
 -- <https://docs.oracle.com/javase/specs/jvms/se7/html/jvms-4.html#jvms-4.1 4.1. The ClassFile Structure>
 data Version =
-  Version
-    Word16
-    Word16
+  Version {
+    _version1 ::
+      Word16
+  , _version2 ::
+      Word16
+  }
   deriving (Eq, Ord, Show)
+
+makeClassy ''Version
 
 data VersionError =
   VersionMinorUnexpectedEof
   | VersionMajorUnexpectedEof
   deriving (Eq, Ord, Show)
 
-{-
-class AsVersionMinorUnexpectedEof p f s where
-  _VersionMinorUnexpectedEof :: 
-    Optic' p f s ()
-
-instance (Choice p, Applicative f) => AsVersionMinorUnexpectedEof p f VersionError where
-  _VersionMinorUnexpectedEof =
-    prism'
-      (\() -> VersionMinorUnexpectedEof)
-      (\e -> case e of
-               VersionMinorUnexpectedEof -> Just ()
-               VersionMajorUnexpectedEof -> Nothing)
+makeClassy ''VersionError
+makeClassyPrisms ''VersionError
 
 versionMinorUnexpectedEof ::
-  AsVersionMinorUnexpectedEof Tagged Identity t =>
+  AsVersionError t =>
   t
 versionMinorUnexpectedEof =
   _VersionMinorUnexpectedEof # ()
 
-class AsVersionMajorUnexpectedEof p f s where
-  _VersionMajorUnexpectedEof :: 
-    Optic' p f s ()
-
-instance (Choice p, Applicative f) => AsVersionMajorUnexpectedEof p f VersionError where
-  _VersionMajorUnexpectedEof =
-    prism'
-      (\() -> VersionMajorUnexpectedEof)
-      (\e -> case e of
-               VersionMinorUnexpectedEof -> Nothing
-               VersionMajorUnexpectedEof -> Just ())
-
 versionMajorUnexpectedEof ::
-  AsVersionMajorUnexpectedEof Tagged Identity t =>
+  AsVersionError t =>
   t
 versionMajorUnexpectedEof =
   _VersionMajorUnexpectedEof # ()
 
-version ::
-  (AsVersionMinorUnexpectedEof Tagged Identity e, AsVersionMajorUnexpectedEof Tagged Identity e) => 
+getVersion ::
+  AsVersionError e =>
   Get e Version
-version =
+getVersion =
   do mn <- versionMinorUnexpectedEof !- word16be
      mj <- versionMajorUnexpectedEof !- word16be
      return (Version mn mj)
--}
