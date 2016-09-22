2a81315509311fd25e794ad8414054db65e4e971
Tony Morris <tmorris@tmorris.net>
2016-09-22 14:37:06 +1000

    Attribute

--- src/Language/Java/Class/Attribute.hs
+++ src/Language/Java/Class/Attribute.hs
@@ -1,46 +1,47 @@
 {-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TemplateHaskell #-}
 
-module Language.Java.Class.Attribute {-(
+module Language.Java.Class.Attribute(
   Attribute(..)
+, HasAttribute(..)
 , AttributeError(..)
-, AsAttributeNameIndexUnexpectedEof(..)
+, HasAttributeError(..)
+, AsAttributeError(..)
+, attributeUnexpectedEof
 , attributeNameIndexUnexpectedEof
-, AsAttributeLengthUnexpectedEof(..)
 , attributeLengthUnexpectedEof
-, AsAttributeUnexpectedEof(..)
-, attribute
-) -} where
+, getAttribute
+) where
 
-import Control.Applicative(Applicative)
-import Control.Lens(AsEmpty, Cons, Optic', Choice, prism', ( # ))
-import Control.Monad(Monad(return))
 import Control.Replicate(replicateO)
-import Data.Eq(Eq)
-import Data.Functor.Identity(Identity)
-import Data.Maybe(Maybe(Just, Nothing))
-import Data.Ord(Ord)
-import Data.Tagged(Tagged)
 import Data.Tickle(Get, (!-), word32be, word16be, word8)
 import Data.Word(Word8, Word16, Word32)
-import Prelude(Show)
+import Papa
 
 -- |
 --
 -- <https://docs.oracle.com/javase/specs/jvms/se7/html/jvms-4.html#jvms-4.7 4.7. Attributes>
 data Attribute a =
-  Attribute
-    Word16 -- attribute_name_index
-    Word32 -- attribute_length
-    (a Word8)
+  Attribute {
+    _attribute_name_index ::
+      Word16
+  , _attribute_length ::
+      Word32
+  , _attribute_info ::
+      a Word8
+  }
 
 deriving instance Eq (a Word8) => Eq (Attribute a)
 deriving instance Ord (a Word8) => Ord (Attribute a)
 deriving instance Show (a Word8) => Show (Attribute a)
+
+makeClassy ''Attribute
 
 data AttributeError =
   AttributeNameIndexUnexpectedEof
@@ -48,64 +49,33 @@ data AttributeError =
   | AttributeUnexpectedEof Word32
   deriving (Eq, Ord, Show)
 
-{-
-class AsAttributeNameIndexUnexpectedEof p f s where
-  _AttributeNameIndexUnexpectedEof :: 
-    Optic' p f s ()
+makeClassy ''AttributeError
+makeClassyPrisms ''AttributeError
 
-instance (Choice p, Applicative f) => AsAttributeNameIndexUnexpectedEof p f AttributeError where
-  _AttributeNameIndexUnexpectedEof =
-    prism'
-      (\() -> AttributeNameIndexUnexpectedEof)
-      (\e -> case e of
-               AttributeNameIndexUnexpectedEof -> Just ()
-               _ -> Nothing)
-       
+attributeUnexpectedEof ::
+  AsAttributeError t =>
+  Word32
+  -> t  
+attributeUnexpectedEof =
+  (_AttributeUnexpectedEof #)
+
 attributeNameIndexUnexpectedEof ::
-  AsAttributeNameIndexUnexpectedEof Tagged Identity t =>
+  AsAttributeError t =>
   t
 attributeNameIndexUnexpectedEof =
   _AttributeNameIndexUnexpectedEof # ()
 
-class AsAttributeLengthUnexpectedEof p f s where
-  _AttributeLengthUnexpectedEof :: 
-    Optic' p f s ()
-
-instance (Choice p, Applicative f) => AsAttributeLengthUnexpectedEof p f AttributeError where
-  _AttributeLengthUnexpectedEof =
-    prism'
-      (\() -> AttributeLengthUnexpectedEof)
-      (\e -> case e of
-               AttributeLengthUnexpectedEof -> Just ()
-               _ -> Nothing)
-       
 attributeLengthUnexpectedEof ::
-  AsAttributeLengthUnexpectedEof Tagged Identity t =>
+  AsAttributeError t =>
   t
 attributeLengthUnexpectedEof =
   _AttributeLengthUnexpectedEof # ()
-
-class AsAttributeUnexpectedEof p f s where
-  _AttributeUnexpectedEof :: 
-    Optic' p f s Word32
-
-instance (Choice p, Applicative f) => AsAttributeUnexpectedEof p f AttributeError where
-  _AttributeUnexpectedEof =
-    prism'
-      AttributeUnexpectedEof
-      (\e -> case e of
-               AttributeUnexpectedEof w -> Just w
-               _ -> Nothing)
-       
-attribute ::
-  (AsEmpty (a Word8), Cons (a Word8) (a Word8) Word8 Word8,
-  AsAttributeNameIndexUnexpectedEof Tagged Identity e,
-  AsAttributeLengthUnexpectedEof Tagged Identity e,
-  AsAttributeUnexpectedEof Tagged Identity e) =>
+     
+getAttribute ::
+  (AsAttributeError e, Cons (a Word8) (a Word8) Word8 Word8, AsEmpty (a Word8)) =>
   Get e (Attribute a)
-attribute =
+getAttribute =
   do n <- attributeNameIndexUnexpectedEof !- word16be
      l <- attributeLengthUnexpectedEof !- word32be
      a <- replicateO (\x -> _AttributeUnexpectedEof # x !- word8) l
      return (Attribute n l a)
--}
