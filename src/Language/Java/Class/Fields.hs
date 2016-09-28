8b084af8246a17f682e7da7a04b5690adfb34a8e
Tony Morris <tmorris@tmorris.net>
2016-09-28 15:31:49 +1000

    Fields

--- src/Language/Java/Class/Fields.hs
+++ src/Language/Java/Class/Fields.hs
@@ -1,35 +1,26 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE GADTs #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE FunctionalDependencies #-}
 
-module Language.Java.Class.Fields {- (
+module Language.Java.Class.Fields(
   Fields(..)
+, HasFields(..)
 , FieldsError(..)
-, AsFieldsFieldInfoUnexpectedEof(..)
-, fieldsFieldInfoUnexpectedEof
-, AsFieldsFieldError(..)
-, fields
-) -} where
+, HasFieldsError(..)
+, AsFieldsError(..)
+, getFields
+) where
 
-import Control.Applicative(Applicative)
-import Control.Category((.))
-import Control.Lens(Cons, AsEmpty, Optic', Choice, prism', ( # ), _2)
-import Control.Monad(Monad(return))
 import Control.Replicate(replicateO)
-import Data.Eq(Eq)
-import Data.Functor.Identity(Identity)
-import Data.Maybe(Maybe(Just, Nothing))
-import Data.Ord(Ord)
-import Data.Tagged(Tagged)
 import Data.Tickle(Get, (!!-), (!-), word16be)
 import Data.Word(Word8, Word16)
-import Language.Java.Class.Attribute -- (AsAttributeNameIndexUnexpectedEof(_AttributeNameIndexUnexpectedEof), AsAttributeLengthUnexpectedEof(_AttributeLengthUnexpectedEof), AsAttributeUnexpectedEof(_AttributeUnexpectedEof), Attribute)
-import Language.Java.Class.Field -- (AsFieldErrorAttributeError(_FieldErrorAttributeError), AsFieldFieldAccessFlagsError(_FieldFieldAccessFlagsError), AsFieldNameIndexUnexpectedEof(_FieldNameIndexUnexpectedEof), AsFieldDescriptorIndexUnexpectedEof(_FieldDescriptorIndexUnexpectedEof), AsFieldAttributeCountUnexpectedEof(_FieldAttributeCountUnexpectedEof), AsFieldAttributeError(_FieldAttributeError), Field, FieldError, field)
-import Prelude(Show)
+import Language.Java.Class.Attribute(Attribute)
+import Language.Java.Class.Field(Field, FieldError, getField)
+import Papa
 
 -- |
 --
@@ -38,6 +29,8 @@ data Fields a f s =
   Fields
     Word16
     (s (Field a f))
+
+makeClassy ''Fields
 
 deriving instance Eq (s (Field a f)) => Eq (Fields a f s)
 deriving instance Ord (s (Field a f)) => Ord (Fields a f s)
@@ -48,85 +41,13 @@ data FieldsError =
   | FieldsFieldError Word16 FieldError
   deriving (Eq, Ord, Show)
 
-{-
-class AsFieldsFieldInfoUnexpectedEof p f s where
-  _FieldsFieldInfoUnexpectedEof :: 
-    Optic' p f s ()
+makeClassy ''FieldsError
+makeClassyPrisms ''FieldsError
 
-instance (Choice p, Applicative f) => AsFieldsFieldInfoUnexpectedEof p f FieldsError where
-  _FieldsFieldInfoUnexpectedEof =
-    prism'
-      (\() -> FieldsFieldInfoUnexpectedEof)
-      (\e -> case e of
-               FieldsFieldInfoUnexpectedEof -> Just ()
-               _ -> Nothing)
-
-fieldsFieldInfoUnexpectedEof ::
-  AsFieldsFieldInfoUnexpectedEof Tagged Identity t =>
-  t
-fieldsFieldInfoUnexpectedEof=
-  _FieldsFieldInfoUnexpectedEof # ()
-
-class AsFieldsFieldError p f s where
-  _FieldsFieldError :: 
-    Optic' p f s (Word16, FieldError)
-
-instance (Choice p, Applicative f) => AsFieldsFieldError p f FieldsError where
-  _FieldsFieldError =
-   prism'
-      (\(w, r) -> FieldsFieldError w r)
-      (\e -> case e of
-               FieldsFieldError w r -> Just (w, r)
-               _ -> Nothing)
-
-instance (p ~ (->), Applicative f) => AsFieldFieldAccessFlagsError p f FieldsError where
-  _FieldFieldAccessFlagsError =
-    _FieldsFieldError . _2 . _FieldFieldAccessFlagsError
-
-instance (p ~ (->), Applicative f) => AsFieldNameIndexUnexpectedEof p f FieldsError where
-  _FieldNameIndexUnexpectedEof =
-    _FieldsFieldError . _2 . _FieldNameIndexUnexpectedEof
-
-instance (p ~ (->), Applicative f) => AsFieldDescriptorIndexUnexpectedEof p f FieldsError where
-  _FieldDescriptorIndexUnexpectedEof =
-    _FieldsFieldError . _2 . _FieldDescriptorIndexUnexpectedEof
-
-instance (p ~ (->), Applicative f) => AsFieldAttributeCountUnexpectedEof p f FieldsError where
-  _FieldAttributeCountUnexpectedEof =
-    _FieldsFieldError . _2 . _FieldAttributeCountUnexpectedEof
-
-instance (p ~ (->), Applicative f) => AsFieldAttributeError p f FieldsError where
-  _FieldAttributeError =
-    _FieldsFieldError . _2 . _FieldAttributeError
-
-instance (p ~ (->), Applicative f) => AsFieldErrorAttributeError p f FieldsError where
-  _FieldErrorAttributeError =
-    _FieldsFieldError . _2 . _FieldErrorAttributeError
-
-instance (p ~ (->), Applicative f) => AsAttributeNameIndexUnexpectedEof p f FieldsError where
-  _AttributeNameIndexUnexpectedEof =
-    _FieldsFieldError . _2 . _AttributeNameIndexUnexpectedEof
-
-instance (p ~ (->), Applicative f) => AsAttributeLengthUnexpectedEof p f FieldsError where
-  _AttributeLengthUnexpectedEof =
-    _FieldsFieldError . _2 . _AttributeLengthUnexpectedEof
-
-instance (p ~ (->), Applicative f) => AsAttributeUnexpectedEof p f FieldsError where
-  _AttributeUnexpectedEof =
-    _FieldsFieldError . _2 . _AttributeUnexpectedEof
-
-fields ::
-  (AsEmpty (f (Attribute a1)), AsEmpty (a Word8),
-    AsEmpty (s (Field a2 f1)),
-    Cons
-      (f (Attribute a1)) (f (Attribute a1)) (Attribute a) (Attribute a),
-    Cons (a Word8) (a Word8) Word8 Word8,
-    Cons (s (Field a2 f1)) (s (Field a2 f1)) (Field a1 f) (Field a1 f),
-    AsFieldsFieldInfoUnexpectedEof Tagged Identity e,
-    AsFieldsFieldError Tagged Identity e) =>
+getFields ::
+  (AsFieldsError e, Cons (s (Field a2 f1)) (s (Field a2 f1)) (Field a1 f) (Field a1 f), Cons (a Word8) (a Word8) Word8 Word8, Cons (f (Attribute a1)) (f (Attribute a1)) (Attribute a) (Attribute a), AsEmpty (s (Field a2 f1)), AsEmpty (a Word8), AsEmpty (f (Attribute a1))) =>
   Get e (Fields a2 f1 s)
-fields =
-  do c <- fieldsFieldInfoUnexpectedEof !- word16be
-     i <- (_FieldsFieldError #) !!- replicateO (\n -> ((,) n) !!- field) c
-     return (Fields c i)
--}
+getFields =
+  do  c <- (_FieldsFieldInfoUnexpectedEof # ()) !- word16be
+      i <- (_FieldsFieldError #) !!- replicateO (\n -> ((,) n) !!- getField) c
+      return (Fields c i)
